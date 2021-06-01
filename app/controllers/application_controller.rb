5af326c97ef7483be1bdba5d9f2046c6c4554824
Beverly Chan <beverlyc.uk@gmail.com>
2021-06-01 15:38:23 +0100

    resolved conflict in app file, edited home file with cards wip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 class ApplicationController < ActionController::Base
-<<<<<<< HEAD
-<<<<<<< HEAD
   before_action :authenticate_user!
 
-=======
-=======
-  before_action :authenticate_user!
->>>>>>> master
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
@@ -16,5 +10,4 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
->>>>>>> master
 end
