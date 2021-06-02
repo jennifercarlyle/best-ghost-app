3b71760b8b8d72ff2e86dd6438f646ce5924ff9b
Jennifer Carlyle <jennifercarlyle@gmail.com>
2021-06-02 12:39:46 +0100

    Add bookings to user profile page and edit devise links

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     set_user
+    @future_bookings = Booking.where("end_date < ? ", Date.today)
+    @completed_bookings = Booking.where("end_date >= ? ", Date.today)
   end
 
   private
