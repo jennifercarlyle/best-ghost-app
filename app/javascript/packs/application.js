f0d0d99a34a5feea0654fb16ae9c0e46ba74fad5
Jennifer Carlyle <jennifercarlyle@gmail.com>
2021-06-03 15:49:13 +0100

    Fix the turbolinks issue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,10 +30,8 @@ import { initStarRating } from '../plugins/init_star_rating';
 // import { initSelect2 } from '../components/init_select2';
 
 
-initStarRating();
-
-
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
+  initStarRating();
   // initSelect2();
 });
